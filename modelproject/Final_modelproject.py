0e9133be0ccd140cf4209bdbb1fbe8689c8b8cba
tgr401 <tgr401@alumni.ku.dk>
2023-05-04 16:37:42 +0200

    test

---
+++ modelproject/Final_modelproject.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy as sp
+from scipy import optimize
+import sympy as sm
+import matplotlib.pyplot as plt
+
+
+
+import ipywidgets as widgets
+
+import mp
+
+class IS_LM:
+    def test():
+        1+1
